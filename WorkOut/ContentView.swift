d0bef14a456d22beeaaf1ffcf44dae0360cb5cb7
Vanessa Bergen <vanessa.bergen1@gmail.com>
2020-06-05 11:39:45 -0400

    Initial Commit

---
+++ WorkOut/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WorkOut
+//
+//  Created by Vanessa Bergen on 2020-06-05.
+//  Copyright © 2020 Vanessa Bergen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
